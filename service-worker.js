2947b68fd43eace14832a9e1c4008be7abe6b291
Dijah545 <62296585+Dijah545@users.noreply.github.com>
2026-08-21 12:10:33 -0500

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='paycycle-budget-full-v1';
+const CACHE='paycycle-budget-ux-v2';
 const ASSETS=['./','./index.html','./css/styles.css','./js/app.js','./js/database.js','./js/paycycle.js','./manifest.json','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
