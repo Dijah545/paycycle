535c5b33198db8280b4724ab6f93018572b375a7
Dijah545 <62296585+Dijah545@users.noreply.github.com>
2026-08-21 20:59:26 -0500

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='khadijah-budget-credit-card-v12';
+const CACHE='khadijah-budget-credit-card-qar-v13';
 const ASSETS=['./','./index.html','./css/styles.css','./manifest.json','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))).then(()=>self.clients.claim())));
